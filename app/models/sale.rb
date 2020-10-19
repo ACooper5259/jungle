4e4947cb5ea875e3a236d7504797c03a04f7fe3c
ACooper5259 <audrey_cooper@me.com>
2020-10-18 19:02:50 -0700

    Sales display in customer view added

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,13 @@
 class Sale < ActiveRecord::Base
 
+  # alternate syntax for Active Records Scope
+  # scope :active, -> { where ("sales.starts_on < ? AND sales.ends_on >= ?", Date.current, Date.current)}
+# creating class methods, Active Records Scope
+  def self.active
+    # add a query (where)
+    where("sales.starts_on < ? AND sales.ends_on >= ?", Date.current, Date.current)
+  end
+
   def finished?
     self.ends_on < Date.current
   end
