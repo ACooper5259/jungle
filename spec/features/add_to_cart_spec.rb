1949859d448005fdb3fccc5db1dc79941d9cb4b0
ACooper5259 <audrey_cooper@me.com>
2020-10-20 19:03:26 -0700

    add_to_cart feature testing implemented

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor adds an item to cart, and the cart in the nav gets updated", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+
+  scenario "They add 1 product to the cart" do
+    # ACT
+    visit '/'
+    first("article.product").click_button('Add')
+    click_link('My Cart (1)')
+
+    # DEBUG 
+    save_screenshot
+    
+    #VERIFY
+    expect(page).to have_content('TOTAL')
+  end
+
+end
