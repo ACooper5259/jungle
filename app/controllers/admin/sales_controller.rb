263f71a90cd040d71421c9fef2e3f96265a9515d
Audrey <audrey_cooper@me.com>
2020-10-21 10:32:34 -0700

    Admin abstract controller implemented for http pasword

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,4 +1,4 @@
-class Admin::SalesController < ApplicationController
+class Admin::SalesController < Admin::BaseController
   def index
     @sales = Sale.all
     # render :index is implicit in Rails so no line of codes means render index
