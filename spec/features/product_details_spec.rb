515832d569ec56e3743611563485444986224d56
ACooper5259 <audrey_cooper@me.com>
2020-10-20 18:51:14 -0700

    feature test for product page implemented

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to specific product page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+
+  scenario "They see 1 product info page" do
+    # ACT
+    visit '/'
+    first("article.product").click_link("Details")
+
+  
+    # DEBUG 
+    save_screenshot
+    
+    #VERIFY
+    expect(page).to have_content('Apparel')
+  end
+
+end
